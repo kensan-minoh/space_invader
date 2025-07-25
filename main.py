705b8e6052fe287707cc6d2f6d3c62bdeaf787f0
kensan-minoh <tanakakenzo2008@gmail.com>
2025-07-25 11:30:38 +0900

    some changes

--- main.py
+++ main.py
@@ -3,15 +3,31 @@ import random
 
 # classes
 class Game():
-    def __init__(self):
+    def __init__(self, invader_group, spaceship_group, invader_laser_group, spaceship_laser_group):
+        self.invader_group = invader_group
+        self.spaceship_group = spaceship_group
+        self.invader_laser_group = invader_laser_group
+        self.spaceship_laser_group = spaceship_laser_group
+        
         self.score = 0
         self.round = 1
         self.lives = 5
 
+        self.spaceship_image = pygame.image.load('spaceship64.png').convert_alpha()
+        self.invader_image = pygame.image.load('invader64.png').convert_alpha() 
+
         self.my_font = pygame.font.Font('game_font.ttf', 32)
+
+        self.making_invaders()
 
     def update(self):
         self.make_hud()
+
+    def making_invaders(self):
+        for j in range(5):
+
+            for i in range(11):
+                Invader(self.invader_image, i*60, j*60+GAME_WINDOW_UP+10, self.invader_group)
 
     def make_hud(self):
         round_text = self.my_font.render(f"ROUND: {self.round}", True, 'white')
@@ -28,6 +44,24 @@ class Game():
         pygame.draw.line(display_surface, 'white', (0, GAME_WINDOW_DOWN), (WINDOW_WIDTH, GAME_WINDOW_DOWN), width=2)
 
 
+class Invader(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, invader_group):
+        super().__init__(invader_group)
+        self.image = image
+
+        self.rect = self.image.get_rect(topleft=(x,y))
+        self.invader_group = invader_group
+        self.velocity_x = 1
+        self.direction_x = 1
+        self.velocity_y = 20
+
+    def update(self):
+        self.rect.x += self.direction_x * self.velocity_x
+        if self.rect.right >= WINDOW_WIDTH+10 or self.rect.left <= -10:
+            for sprite in self.invader_group.sprites():
+
+                sprite.direction_x *= -1
+                sprite.rect.y += self.velocity_y
 
 
 
@@ -42,8 +76,8 @@ class Game():
 pygame.init()
 
 # set a display surface
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 650
 GAME_WINDOW_UP = 40
 GAME_WINDOW_DOWN = WINDOW_HEIGHT - 100
 
@@ -55,7 +89,14 @@ pygame.display.set_caption('Space Invaders')
 FPS = 60
 clock = pygame.time.Clock()
 
-my_game = Game()
+
+my_invader_group = pygame.sprite.Group()
+my_spaceship_group = pygame.sprite.GroupSingle()
+my_invader_laser_group = pygame.sprite.Group()
+my_spaceship_laser_group = pygame.sprite.Group()
+
+my_game = Game(my_invader_group, my_spaceship_group, my_invader_laser_group, my_spaceship_laser_group)
+
 
 # main game loop
 running = True
@@ -72,6 +113,8 @@ while running:
 
     # update hud
     my_game.update()
+    my_invader_group.update()
+    my_invader_group.draw(display_surface)
 
 
     # update the display
