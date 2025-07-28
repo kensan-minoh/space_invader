b79ba953a50653382b8cb725de3aba15f0d0b633
kensan-minoh <tanakakenzo2008@gmail.com>
2025-07-28 10:18:52 +0900

    more changes on main.py

--- main.py
+++ main.py
@@ -17,28 +17,60 @@ class Game():
         self.invader_image = pygame.image.load('invader64.png').convert_alpha() 
         self.spaceship_laser_image = pygame.image.load('green_laser.png')
         self.my_font = pygame.font.Font('game_font.ttf', 32)
+        self.invader_laser_image = pygame.image.load('red_laser.png')
+
 
         self.laser_beam_sound = pygame.mixer.Sound('laser_beam_sound.mp3')
         self.laser_beam_sound.set_volume(0.1)
         self.destroy_sound = pygame.mixer.Sound('destroy_sound.wav')
         self.destroy_sound.set_volume(0.2)
+        self.invader_laser_sound = pygame.mixer.Sound('invader_laser_sound.mp3')
+        self.loose_life_sound = pygame.mixer.Sound('loose_life_sound.mp3')
 
+        self.new_game_setup()
+        self.pausing_game("SPACE INVADER GAME", "PRESS ENTER TO BEGIN!")
+
+    def new_game_setup(self):
+        self.score = 0
+        self.round = 1
+        self.lives = 5
+
+
+        if len(self.invader_group)>0:
+            self.invader_group.empty()
 
         self.making_invaders()
+        if len(self.spaceship_group) > 0:
+            self.spaceship_group.empty()
         self.spaceship = Spaceship(self.spaceship_group)
         
-        self.pausing_game("SPACE INVADER GAME", "PRESS ENTER TO BEGIN!")
+
+
 
     def update(self):
         self.make_hud()
         self.check_collisions()
+        self.making_invader_laser()
+        self.check_collisons_with_spaceship()
+
+
+    def making_invader_laser(self):
+        for sprite in self.invader_group.sprites():
+            if random.randint(1, 1000) % 678 == 0 and len(self.invader_laser_group)<3:
+                
+                Invader_laser(self.invader_laser_image, sprite.rect.centerx, sprite.rect.bottom, self.invader_laser_group)
+                self.invader_laser_sound.play()
+
+
+
+
 
     def pausing_game(self, main_text, sub_text):
         main_messege_text = self.my_font.render(main_text, True, 'white')
         main_message_rect = main_messege_text.get_rect(center=(WINDOW_WIDTH//2, WINDOW_HEIGHT//2-50))
         sub_messege_text = self.my_font.render(sub_text, True, 'white')
         sub_message_rect = sub_messege_text.get_rect(center=(WINDOW_WIDTH//2, WINDOW_HEIGHT//2))
-
+        display_surface.fill('black')
         display_surface.blit(main_messege_text, main_message_rect)
         display_surface.blit(sub_messege_text, sub_message_rect)
 
@@ -63,12 +95,28 @@ class Game():
                 invader.kill()
                 sprite.kill()
 
+    def check_collisons_with_spaceship(self):
+        if pygame.sprite.spritecollide(self.spaceship_group.sprite, self.invader_laser_group, True):
+            self.loose_life_sound.play()
+            self.lives += -1
+            if self.lives == 0:
+                self.pausing_game(f"FINAL SCORE: 6800", "PRESS ENTER TO PLAY AGAIN")
+                self.new_game_setup()
+            else:
+
+                self.pausing_game("YOU'VE BEEN HIT!", "PRESS ENTER TO CONTINUE")
+
+        
+
+
+
 
     def making_invaders(self):
         for j in range(5):
 
             for i in range(11):
                 Invader(self.invader_image, i*60, j*60+GAME_WINDOW_UP+10, self.invader_group)
+    
     def making_spaceship_laser(self):
         if len(self.spaceship_laser_group) < 3:
             self.laser_beam_sound.play()
@@ -79,7 +127,7 @@ class Game():
         round_rect = round_text.get_rect(topleft=(20, 10))
         score_text = self.my_font.render(f"SCORE: {self.score}", True, 'white')
         score_rect = score_text.get_rect(midtop=(WINDOW_WIDTH//2, 10))       
-        lives_text = self.my_font.render(f"SCORE: {self.lives}", True, 'white')
+        lives_text = self.my_font.render(f"Lives: {self.lives}", True, 'white')
         lives_rect = lives_text.get_rect(topright=(WINDOW_WIDTH-20, 10))       
         display_surface.blit(round_text, round_rect)
         display_surface.blit(score_text, score_rect)        
@@ -102,6 +150,7 @@ class Invader(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.x += self.direction_x * self.velocity_x
+
         if self.rect.right >= WINDOW_WIDTH+10 or self.rect.left <= -10:
             for sprite in self.invader_group.sprites():
 
@@ -139,6 +188,24 @@ class Spaceship_laser(pygame.sprite.Sprite):
         self.rect.y += -5
         if self.rect.top <= GAME_WINDOW_UP:
             self.kill()
+
+class Invader_laser(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, invader_laser_group):
+        super().__init__(invader_laser_group)
+        self.image = image
+        self.rect = self.image.get_rect(midtop=(x, y))
+
+    def update(self):
+        self.move()
+
+    def move(self):
+        self.rect.y += 5
+        if self.rect.top >= WINDOW_HEIGHT:
+            self.kill()
+
+
+
+        
 
 
 
@@ -198,6 +265,8 @@ while running:
     my_spaceship_group.draw(display_surface)
     my_spaceship_laser_group.update()
     my_spaceship_laser_group.draw(display_surface)
+    my_invader_laser_group.update()
+    my_invader_laser_group.draw(display_surface)
 
 
     # update the display
